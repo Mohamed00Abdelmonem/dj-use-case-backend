e5f547a95ce518b5ac7cbaec5eeff4c228840c65
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2026-09-10 12:31:36 +0300

    feat: initialize gaps app routing and define project dependencies

---
+++ apps/gaps/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+# from apps.gaps.views import GapClassificationViewSet, GapViewSet
+
+# urlpatterns = [
+#     path('gap-classifications/', GapClassificationViewSet.as_view({'get': 'list', 'post': 'create'})),
+#     path('gap-classifications/<int:pk>/', GapClassificationViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
+#     path('gaps/', GapViewSet.as_view({'get': 'list', 'post': 'create'})),
+#     path('gaps/<int:pk>/', GapViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
+# ]
